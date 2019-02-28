b0db446814edc339611e5d3100d041e99860e332
hrishi2710 <hrishirules2710@gmail.com>
2019-02-28 21:05:13 +0530

    Revert "native html syntax for slam skeleton" reverting the changes on master branch This reverts commit 4374bf62d5eb9148f93de0b0f62b365daf5a8974.

--- src/App.js
+++ src/App.js
@@ -6,106 +6,106 @@ import "react-datepicker/dist/react-datepicker.css";
 
 
 class App extends Component {
-  
   render() {
     return (
-      <div className="container">
-        <div className="row">
-          <div className="col-lg-6 offset-lg-3">
-            <div>
-              <h1>Slambook</h1>
-            </div>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col">
-            <form className="wholeform">
-              <div className="form-group row">
-                <label htmlFor="name" className="form-label col-form-label col-sm-2">Name:</label>
-                <div className="col-sm-10">
-                  <input placeholder="Enter your name" type="name" id="name" className="form-control form-control-sm"/>
-                </div>
-              </div>
-              <div className="form-group row">
-                <label htmlFor="dob" className="form-label col-form-label col-sm-2">DOB:</label>
-                <div className="col-sm-10">
-                  <div className="react-datepicker-wrapper">
-                    <div className="react-datepicker__input-container">
-                      <input type="text" className="" />
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="form-group row">
-                <label htmlFor="contact" className="form-label col-form-label col-sm-2">Contact:</label>
-                <div className="col-sm-10">
-                  <input placeholder="Enter your Contact details" type="contact" id="contact" className="form-control form-control-sm"/>
-                </div>
-                <small className="contact-muted-text form-text">Your contact is confidential! Cheers!</small>
-              </div>
-              <div className="form-group row">
-                <label htmlFor="gender" className="form-label col-form-label col-sm-2">Gender:</label>
-                <div className="col-sm-10">
-                  <div className="dropdown">
-                    <button aria-haspopup="true" aria-expanded="false" id="gender-dropdown" type="button" className="dropdown-toggle btn btn-success">Select gender</button>
-                    <div aria-labelledby="gender-dropdown" className="dropdown-menu" x-placement="bottom-start" style={{"position": "absolute", "willchange": "transform", "top":"0px", "left": "0px", "transform": "translate3d(0px, 38px, 0px)"}}>
-                      <a href="#/Male" className="dropdown-item">Male</a>
-                      <a href="#/Female" className="dropdown-item">Female</a>
-                      <a href="#/Transgender" className="dropdown-item">Transgender</a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="form-group row">
-                <label htmlFor="zodiac" className="form-label col-form-label col-sm-2">Zodiac:</label>
-                <div className="col-sm-10">
-                  <input placeholder="Enter your Zodiac" type="zodiac" id="zodiac" className="form-control form-control-sm"/>
-                </div>
-              </div>
-            </form>
-          </div>
-          <div className="col">
-            <form className="wholeform">
-            <div className="form-group row">
-              <label htmlFor="crush" className="form-label col-form-label col-sm-2">1st crush:</label>
-              <div className="col-sm-10">
-                <input type="crush" id="crush" className="form-control form-control-sm"/>
-              </div>
-            </div>
-            <div className="form-group row">
-              <label htmlFor="sport" className="form-label col-form-label col-sm-2">Fav sport:</label>
-              <div className="col-sm-10">
-                <input placeholder="Cricket, football etc." type="sport" id="sport" className="form-control form-control-sm"/>
-              </div>
-            </div>
-            <div className="form-group row">
-              <label htmlFor="hobby" className="form-label col-form-label col-sm-2">Hobby:</label>
-              <div className="col-sm-10">
-                <input placeholder="video games, coin collection etc." type="hobby" id="hobby" className="form-control form-control-sm"/>
-              </div>
-            </div>
-            <div className="form-group row">
-              <label htmlFor="nickname" className="form-label col-form-label col-sm-2">Nickname:</label>
-              <div className="col-sm-10">
-                <input placeholder="what friends call you!" type="nickname" id="nickname" className="form-control form-control-sm"/>
-              </div>
-            </div>
-            <div className="form-group row">
-              <label htmlFor="about-me" className="form-label col-form-label col-sm-2">About me:</label>
-              <div className="col-sm-10">
-                <textarea rows="5" placeholder="Write whatever comes to your mind!" type="about-me" id="about-me" className="form-control form-control-sm"></textarea>
-              </div>
-            </div>
-          </form>
-        </div>
-      </div>
-      <div className="row">
-        <button type="submit" className="btn btn-primary btn-lg">Submit</button>
-      </div>
-    </div>     
+      <Container>
+        <Row>
+          <Col lg={{ span :6 , offset :3}}>
+          <div><h1>Slambook</h1></div>
+          </Col>
+        </Row>
+        {/* column 1 form starts here */}
+        <Row>
+          <Col>
+          <Form className="wholeform">
+              <Form.Group as={Row} controlId="name" >
+              <Form.Label  column sm="2">Name:</Form.Label>
+              <Col sm="10"><Form.Control type="name" placeholder="Enter your name" size="sm" />
+              </Col>
+              </Form.Group>
+
+              <Form.Group as={Row} controlId="dob" >
+              <Form.Label  column sm="2">DOB:</Form.Label>
+              <Col sm="10"><DatePicker/>
+              </Col>
+              </Form.Group>
+
+              <Form.Group as={Row} controlId="contact" >
+              <Form.Label column sm="2" >Contact:</Form.Label>
+              <Col sm="10"><Form.Control type="contact" placeholder="Enter your Contact details" size="sm" />
+              </Col>
+              <Form.Text className='contact-muted-text'>Your contact is confidential! Cheers!</Form.Text>
+              </Form.Group>
+
+              <Form.Group as={Row} controlId="gender" >
+              <Form.Label column sm="2" >Gender:</Form.Label>
+              <Col sm="10">
+              <Dropdown>
+                <Dropdown.Toggle variant="success" id="gender-dropdown">Select gender</Dropdown.Toggle>
+                <Dropdown.Menu>
+                  <Dropdown.Item href="#/Male">Male</Dropdown.Item>
+                  <Dropdown.Item href="#/Female">Female</Dropdown.Item>
+                  <Dropdown.Item href="#/Transgender">Transgender</Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+              </Col>
+              </Form.Group>
+
+              <Form.Group as={Row} controlId="zodiac" >
+              <Form.Label column sm="2" >Zodiac:</Form.Label>
+              <Col sm="10"><Form.Control type="zodiac" placeholder="Enter your Zodiac" size="sm" />
+              </Col>
+              </Form.Group>
+            </Form>
+          </Col>
+
+          {/* column 1 form ends here */}
+
+          {/* column 2 form starts here */}
+
+          <Col>
+          <Form className="wholeform">
+              <Form.Group as={Row} controlId="crush" >
+              <Form.Label column sm="2" >1st crush:</Form.Label>
+              <Col sm="10"><Form.Control type="crush"  size="sm" />
+              </Col>
+              </Form.Group>
+
+              <Form.Group as={Row} controlId="sport" >
+              <Form.Label column sm="2" >Fav sport:</Form.Label>
+              <Col sm="10"><Form.Control type="sport" placeholder="Cricket, football etc." size="sm" />
+              </Col>
+              </Form.Group>
+
+              <Form.Group as={Row} controlId="hobby" >
+              <Form.Label column sm="2" >Hobby:</Form.Label>
+              <Col sm="10"><Form.Control type="hobby" placeholder="video games, coin collection etc." size="sm" />
+              </Col>
+              </Form.Group>
+
+              <Form.Group as={Row} controlId="nickname" >
+              <Form.Label column sm="2" >Nickname:</Form.Label>
+              <Col sm="10"><Form.Control type="nickname"  size="sm"  placeholder="what friends call you!"/>
+              </Col>
+              </Form.Group>
+
+              <Form.Group as={Row} controlId="about-me" >
+              <Form.Label column sm="2" >About me:</Form.Label>
+              <Col sm="10"><Form.Control as="textarea" rows="5" type="about-me" placeholder="Write whatever comes to your mind!" size="sm" />
+              </Col>
+              </Form.Group>
+            </Form>
+          </Col>
+
+          {/* column 2 form ends here */}
+        </Row>
+        
+        <Row>
+          <Button type="submit" size="lg">Submit</Button>
+        </Row>
+      </Container>     
     );
   }
 }
 
 export default App;
-
